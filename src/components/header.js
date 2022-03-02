aac2ff15a9148f9aa9b17cb8647d70d1a7802323
Ezequiel Bispo <ezequielbispo011@gmail.com>
2022-03-02 04:41:22 -0300

    React Bootstrap Installed

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Header(){
+    return(
+         <Navbar.brand>
+             <img src="https://yata-apix-16ba64c4-9e6e-4003-8496-09e98941c5a9.lss.locawebcorp.com.br/7f230ffa83a14a3a85441cb5de97fb95.png" width="10%" height="10%"></img>
+         </Navbar.brand>
+    )
+}
