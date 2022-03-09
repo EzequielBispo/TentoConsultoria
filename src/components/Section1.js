6f640a647d10f04c7fc57b5fca1c2ec7d62b5c41
Ezequiel Bispo <ezequielbispo011@gmail.com>
2022-03-09 16:18:32 -0300

    Header Altered

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import {Card} from 'react-bootstrap';
+import {Card, Button} from 'react-bootstrap';
 
 export default function section1(){
     return(
+        <>
         <Card body id="section1" className="position-relative">
-        <h1 className="shadow-lg p-3 mb-5 bg-body rounded bg-white border rounded-3 px-5 py-5 overflow-hidden position-absolute top-50 start-50 translate-middle text-center fs-1 text-black">CONSULTORIA TRIBUTÁRIA & SOCIETÁRIA QUALIFICADA</h1>
+        <div className="shadow-lg p-3 mb-5 bg-body rounded bg-white border rounded-3 px-4 py-5 overflow-hidden position-absolute top-50 start-50 translate-middle text-center">
+        <h1 className="overflow-hidden">
+            CONSULTORIA TRIBUTÁRIA & SOCIETÁRIA QUALIFICADA
+            </h1>
+        <Button id="button" variant="danger" className="text-center mt-4 fw-bold">SAIBA MAIS</Button>
+        </div>
         </Card>
+
+        
+        </>
     )
 }
